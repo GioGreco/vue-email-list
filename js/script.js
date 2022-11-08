6d696e251568c4a13eae3ccd0e9565dbdbad1727
GioGreco <grecogiorgio.97@gmail.com>
2022-11-08 16:04:42 +0100

    add simple style

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const app = createApp({
     },
     methods: {
         getMailApi(){
+            this.mailList = [];
             for(let i=0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
                     console.log(resp.data.response);
